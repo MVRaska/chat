aabfb843df44c9a7da33527b0fedf93f7cad999c
MVRaska <143351048+MVRaska@users.noreply.github.com>
2025-05-21 23:39:58 +0200

    translate chat.js

--- chat.js
+++ chat.js
@@ -1,11 +1,11 @@
-// Napraviti chats kolekciju u Firestore, koja će sadržati sledeća polja:
-// documentID (auto increment) – ID poruke koji se automatski dodeljuje
-// message (string) – Tekst poruke
-// usrename (string) – Korisničko ime korisnika koji je napisao poruku
-// room (string) – Soba u kojoj je kreirana poruka
-// created_at (timestamp) – Datum i vreme kreiranja poruke
-// Povezati (konektovati) bazu podataka (kreiranu kolekciju) sa svojim projektom
-
+/* Create a 'chats' collection in Firestore containing the following fields:
+documentID (auto increment) – Message ID assigned automatically
+message (string) – The content of the message
+username (string) – The username of the person who sent the message
+room (string) – The room where the message was posted
+created_at (timestamp) – Date and time the message was created
+Connect the database (the created collection) with your project
+*/
 
 class Chatroom {
     constructor(room, userName) {
@@ -17,89 +17,88 @@ class Chatroom {
 
     set room(r) {
         this._room = r;
-        if(this.unsub) {
+        if (this.unsub) {
             this.unsub();
         }
     }
+
     set userName(un) {
-        if(un.length > 2 && un.length < 10 && un.trim() != '') {
+        if (un.length > 2 && un.length < 10 && un.trim() !== '') {
             this._userName = un;
         } else {
-            alert('Pogresno uneto korisnicko ime');
-        }   
+            alert('Invalid username input');
+        }
     }
 
     get room() {
         return this._room;
     }
+
     get userName() {
         return this._userName;
     }
 
-    async addChat(mess) {
-        // kreiranje dokumenta koji zelimo da dodamo u bazu
-
+    async addChat(message) {
+        // Create a document to be added to the database
         try {
-            let docChat = {
-                message: mess,
+            const chatDoc = {
+                message: message,
                 username: this.userName,
                 room: this.room,
                 created_at: new Date()
             };
 
-            let response = await this.chats.add(docChat);   //pamti dokument u bazi
+            const response = await this.chats.add(chatDoc); // Save the document in the database
 
-            docChat.id = response.id;
+            chatDoc.id = response.id;
             console.log(response.id);
 
-            return { id: response.id, referenca: response};   //vraca promise na koji moze da se doda then() i catch()
+            return { id: response.id, reference: response }; // Returns a promise that can be chained with then() and catch()
+        } catch (err) {
+            console.error('An error occurred:', err);
         }
-        catch {
-            console.error('Doslo je do greske', err);
-        } 
     }
 
     getChats(callback) {
-        // pozivanje dokumenta iz iste sobe iz baze i pracenje dodatih dokumenata
+        // Listen to documents in the current room and track added ones
         this.unsub = this.chats
-        .where('room', '==', this.room)
-        .orderBy('created_at')
-        .onSnapshot(snapShot => {
-            snapShot.docChanges().forEach(change => {
-                if(change.type == 'added') {
-                    let docData = change.doc.data();
-                    docData.id = change.doc.id;
-                    callback(docData);
-                }
+            .where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type === 'added') {
+                        const docData = change.doc.data();
+                        docData.id = change.doc.id;
+                        callback(docData);
+                    }
+                });
             });
-        });
     }
 
-    aktivniKorisnik(aktivan) {
-        // ispis u novom div tagu - obavestenje da je promenjen username
-        let divKorisnickoIme = document.createElement('div');
-        divKorisnickoIme.innerHTML = `Korisnicko ime je promenjeno u "<span style='font-weight: bold;'>${aktivan}</span>"`;
-        document.body.appendChild(divKorisnickoIme);
+    showActiveUser(user) {
+        // Display a message in a new div tag notifying the username change
+        const userDiv = document.createElement('div');
+        userDiv.innerHTML = `Username has been changed to "<span style='font-weight: bold;'>${user}</span>"`;
+        document.body.appendChild(userDiv);
         setTimeout(() => {
-            document.body.removeChild(divKorisnickoIme);
+            document.body.removeChild(userDiv);
         }, 3000);
-    } 
+    }
 
     deleteMsgDB(id) {
-        // brisanje dokumenta iz baze klikom na kanticu
+        // Delete a document from the database by clicking the delete icon
         this.chats.doc(id)
-        .delete()
-        .then(() => {
-            console.log('Dokument uspesno izbrisan iz baze podataka');
-        })
-        .catch(e => {
-            console.log(e => {
-                console.log(`Greska prilikom brisanja dokumenta u bazi podataka: ${e}`);
+            .delete()
+            .then(() => {
+                console.log('Document successfully deleted from the database');
+            })
+            .catch(e => {
+                console.error(`Error while deleting the document from the database: ${e}`);
             });
-        })
     }
 }
 
-export {Chatroom};
+export { Chatroom };
+
 
 
